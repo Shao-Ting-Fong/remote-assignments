7960d1ec976dd6645bdd5744dbaea5b6dfe9d345
ShaoTing Fong <fengshaoting@fengshaotingde-MacBook-Pro.local>
2023-04-12 18:59:48 +0800

    Add flexibility to more alphabets.

--- Week_1/Assignment_3/app.js
+++ Week_1/Assignment_3/app.js
@@ -1,13 +1,15 @@
-function countAandB(input) { // your code here
+function countAandB(input) {
+  // your code here
   let count = 0;
   for (let char of input) {
-    if (char === 'a' || char === 'b') {
+    if (char === "a" || char === "b") {
       count += 1;
     }
   }
   return count;
 }
-function toNumber(input) { // your code here
+function toNumber(input) {
+  // your code here
 
   // Way 1:
   // const alphabet = ['a', 'b', 'c', 'd', 'e'];
@@ -17,19 +19,32 @@ function toNumber(input) { // your code here
   // }
 
   const alphabet = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5,
-  }
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+  };
 
-  return input.map((ele) => { return alphabet[ele] });
+  return input.map((ele) => {
+    return alphabet[ele];
+  });
 }
-let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+
+function toNumber2(input) {
+  return input.map((ele) => {
+    return ele.charCodeAt(0) - 97 + 1;
+  });
+}
+
+let input1 = ["a", "b", "c", "a", "c", "a", "c"];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
 console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
 
-let input2 = ['e', 'd', 'c', 'd', 'e'];
-console.log(countAandB(input2)); // should print 0 
+let input2 = ["e", "d", "c", "d", "e"];
+console.log(countAandB(input2)); // should print 0
 console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
+
+let input3 = ["a", "e", "d", "c", "d", "e", "o", "s", "f"];
+console.log(toNumber2(input2)); // should print [5, 4, 3, 4, 5]
+console.log(toNumber2(input3));
