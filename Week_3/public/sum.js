c0fc4a62f5e3047c15478412a30d0fdd27948d26
ShaoTing Fong <fengshaoting@fengshaotingde-MacBook-Pro.local>
2023-04-13 16:48:45 +0800

    Use async function to handle fetch api.

--- Week_3/public/sum.js
+++ Week_3/public/sum.js
@@ -1,17 +1,29 @@
 const form = document.querySelector("form");
 const result = document.querySelector("#result");
 
-form.addEventListener("submit", (evt) => {
+form.addEventListener("submit", async (evt) => {
   evt.preventDefault();
-  fetch(`http://localhost:3000/data?number=${evt.target[0].value}`)
-    .then((data) => data.json())
-    .then((data) => {
-      if (data.isValid) {
-        result.classList.add("calc-result");
-        result.innerHTML = data.value;
-      } else {
-        result.classList.remove("calc-result");
-        result.innerHTML = [`<div class="alert alert-danger alert-dismissible" role="alert">`, `   <div>${data.message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
-      }
-    });
+  const url = `http://localhost:3000/data?number=${evt.target[0].value}`;
+  //   Way 1: using .then
+  //   fetch(url)
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       if (data.isValid) {
+  //         result.classList.add("calc-result");
+  //         result.innerHTML = data.value;
+  //       } else {
+  //         result.classList.remove("calc-result");
+  //         result.innerHTML = [`<div class="alert alert-danger alert-dismissible" role="alert">`, `   <div>${data.message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
+  //       }
+  //     });
+
+  // Way 2: using async function
+  const data = await (await fetch(url)).json();
+  if (data.isValid) {
+    result.classList.add("calc-result");
+    result.innerHTML = data.value;
+  } else {
+    result.classList.remove("calc-result");
+    result.innerHTML = [`<div class="alert alert-danger alert-dismissible" role="alert">`, `   <div>${data.message}</div>`, '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>', "</div>"].join("");
+  }
 });
